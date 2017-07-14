f91b74145146d53584ea2e2215590715317093f3
alexthkim <atk3@rice.edu>
2017-07-14 16:03:24 -0700

    updated api

--- apiroutes.js
+++ apiroutes.js
@@ -5,6 +5,8 @@ var models = require('./models/models');
 var User = models.User;
 var Photo = models.Photo
 
+
+
 router.post('/login') {
   // Not sure if necessary b/c of facebook Authentication, but we should Check
   // whether or not a user is logged in so that we can create a login wall?
@@ -12,18 +14,43 @@ router.post('/login') {
   // bypass the login screen without logging in
 }
 
+// function generateInitial(userId) {
+//   User.findById(userId, function(err, user) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // Some algo here
+//     }
+//   })
+// }
+
+// Input: req.body.setNumber, req.body.cards
+// On set 1 -> generate 5 images
+// On set 2 -> read the first 4 cards, generate the next 5 images
+// On potential set 3 -> read the first 5 cards, generate the next 5 images
+// On potential set 4 -> read the first 5 cards, generate the next 5 images
+// Generate the first 5 pictures based on some heuristic (max of 15 or 20)
+// Set number 1 = random, 2 = based on 1 [mandatory],
+// Set number 3 = based on 2, 4 = based on 3 [optional if our heuristic is unclear]
+// Return either an object containing two things either:
+// {finished: false, images: []} or {finished: true, keywords: []}
 router.post('/generate/:id', function(req, res) {
-  // Input: req.body.setNumber, req.body.cards
-  // On set 1 -> generate 5 images
-  // On set 2 -> read the first 4 cards, generate the next 5 images
-  // On potential set 3 -> read the first 5 cards, generate the next 5 images
-  // On potential set 4 -> read the first 5 cards, generate the next 5 images
-  // Generate the first 5 pictures based on some heuristic (max of 15 or 20)
-  // Set number 1 = random, 2 = based on 1 [mandatory],
-  // Set number 3 = based on 2, 4 = based on 3 [optional if our heuristic is unclear]
-  // Return either an object containing two things either:
-  // {finished: false, images: []} or {finished: true, keywords: []}
+  var setNum = req.body.setNumber;
+  if (setNum === 1) {
+    return generateInitial(req.query.id);
+  } else (setNum === 2) {
+    return generateAndAnalyze(req.query.id, req.body.cards)
+  }
 })
+
+/*
+  To-Do
+  1. function generateIntial(userId)
+     Takes a userId -> returns an array of 5 image urls
+  2. function generateAndAnalyze(userId, cards)
+     Takes a userId + card mapping to true or false dependent on whether they swiped
+     -> returns an array of 5 image urls or keywords if the heuristic is finished
+*/
 
 router.post('/results/:id', function(req, res) {
   // Input: req.body.keywords
--- models/models.js
+++ models/models.js
@@ -2,29 +2,49 @@
 
 var mongoose = require('mongoose');
 
-var Token = mongoose.model('Token', {
-  userId: String,
-  token: String,
-  createdAt: Date
-});
+var foodSchema = mongoose.Schema({
+  url: String,
+  timeOfDay: {
+    type: String,
+    enum: ["b", "l", "d", "s"]
+  }
+})
 
-var User = mongoose.model('User', {
+var userSchema = mongoose.Schema({
   fname: String,
   lname: String,
+  photoUrl: String,
   email: String,
-  password: String
-});
+  mainPref: Array,
+  sessionPref: Array,
+  explorePref: {
+    type: String,
+    enum: ["comfort","exploring"]
+  }
+})
 
-var Post = mongoose.model('Post', {
-  poster: Object,
-  content: String,
-  likes: Array,
-  comments: Array,
-  createdAt: Date
-});
+var restCategorySchema = mongoose.Schema({
+  restCateogry: String,
+  foods: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Food' }]
+})
+
+var cuisineSchema = mongoose.Schema({
+  cuisine: String,
+  foods: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Food' }]
+})
+
+var Food = mongoose.model('Food', foodSchema);
+var User = mongoose.model('User', userSchema);
+var restCategory = mongoose.model('RestCategory', restCategorySchema);
+var Cuisine = mongoose.model('FoodCategory', cuisineSchema);
 
 module.exports = {
+  Food: Food,
   User: User,
-  Token: Token,
-  Post: Post
+  restCategory: restCategory,
+  Cuisine: Cuisine
 }
